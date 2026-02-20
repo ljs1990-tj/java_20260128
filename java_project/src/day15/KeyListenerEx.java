8c0de3f76ee528918aa1cca6f225a3ca5e0df3f9
tj-bu-708-teacher <tj-bu-708-teacher@DESKTOP-87NNH3H>
2026-02-20 15:00:38 +0900

    2026.02.20

---
+++ java_project/src/day15/KeyListenerEx.java
@@ -0,0 +1,56 @@
+package day15;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class KeyListenerEx extends JFrame {
+	private JLabel[] keyMessage;
+
+	public KeyListenerEx() {
+		setTitle("keyListener 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		c.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				int keyCode = e.getKeyCode();
+				char keyChar = e.getKeyChar();
+				keyMessage[0].setText(Integer.toString(keyCode));
+				keyMessage[1].setText(Character.toString(keyChar));
+				keyMessage[2].setText(e.getKeyText(keyCode));
+			}
+		});
+
+		keyMessage = new JLabel[3];
+		keyMessage[0] = new JLabel(" getKeyCode() ");
+		keyMessage[1] = new JLabel(" getKeyChar() ");
+		keyMessage[2] = new JLabel(" getKeyText() ");
+
+		for (int i = 0; i < keyMessage.length; i++) {
+			c.add(keyMessage[i]);
+			keyMessage[i].setOpaque(true);
+			keyMessage[i].setBackground(Color.YELLOW);
+		}
+
+		setSize(300, 150);
+		setVisible(true);
+		c.setFocusable(true);
+		c.requestFocus();
+
+	};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new KeyListenerEx();
+	}
+
+}
