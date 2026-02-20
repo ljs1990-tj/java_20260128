5dc74e5d7b3e5851a054f07fda564bbdddf419cb
tj-bu-708-teacher <tj-bu-708-teacher@DESKTOP-87NNH3H>
2026-02-20 10:36:15 +0900

    2026.02.20

--- java_project/src/day14/Java9_DB.java
+++ java_project/src/day14/Java9_DB.java
@@ -8,6 +8,21 @@ import day13.DBClass;
 
 public class Java9_DB {
 	
+	public static int inputScore(String subject) {
+		Scanner s = new Scanner(System.in);
+		int score = 0;
+		while(true) {
+			System.out.print(subject + " : ");
+			score = s.nextInt();
+			if(score >= 0 && score <= 100) {
+				break;
+			} else {
+				System.out.println("0~100 사이 값을 입력해주세요.");
+			}
+		}
+		return score;
+	}
+	
 	public static void searchStudent(Statement stmt) {
 		try {
 			String sql = "SELECT * FROM TBL_STUDENT";
@@ -81,7 +96,64 @@ public class Java9_DB {
 		}
 	}
 	
-
+	public static void editStudent(Statement stmt) {
+		Scanner s = new Scanner(System.in);
+		try {
+			String stuNo = "";
+			while(true) {
+				System.out.print("학번 : ");
+				stuNo = s.next();
+				String sql = "SELECT * FROM TBL_STUDENT WHERE STU_NO = '" + stuNo + "'";
+				ResultSet rs = stmt.executeQuery(sql);
+				if(rs.next()) {
+					System.out.print("[ 1. 자바, 2. 오라클, 3. HTML ] : ");
+					int menu = s.nextInt();
+					int score = 0;
+					String subject = "";
+					switch (menu) {
+					case 1:
+						score = inputScore("자바");
+						subject = "JAVA";
+						break;
+					case 2:
+						score = inputScore("오라클");
+						subject = "ORACLE";
+						break;
+					case 3:
+						score = inputScore("HTML");
+						subject = "HTML";
+						break;
+					default:
+						System.out.println("1~3중에 선택해주세요.");
+						break;
+					}
+					
+					sql = "UPDATE TBL_STUDENT SET " 
+							 + subject + " = " + score + " "
+							 + "WHERE STU_NO = '" + stuNo + "'";
+					
+					int result = stmt.executeUpdate(sql);
+					if(result > 0) {
+						System.out.println("수정되었습니다!");
+					} else {
+						System.out.println("수정에 실패했습니다!");
+					}
+					
+					break;
+				} else {
+					System.out.println("없는 학번 입니다.");
+				}
+				
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
@@ -98,7 +170,7 @@ public class Java9_DB {
 			} else if(menu == 2) {
 				addStudent(stmt);
 			} else if(menu == 3) {
-				
+				editStudent(stmt);
 			} else if(menu == 4) {
 				removeStudent(stmt);
 			} else if(menu == 5) {
