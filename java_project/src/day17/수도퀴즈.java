b91d8f75fde3370938634fdbe04c8dac5b5f8acd
tj-bu-708-teacher <tj-bu-708-teacher@DESKTOP-87NNH3H>
2026-02-25 16:39:24 +0900

    2026.02.25 수도 맞추기(객관식)

--- java_project/src/day17/수도퀴즈.java
+++ java_project/src/day17/수도퀴즈.java
@@ -12,6 +12,9 @@ public class 수도퀴즈 {
 		CapitalQuiz obj = new CapitalQuiz();
 		ArrayList<Quiz> quizList = obj.getList();
 		ArrayList<String> questionList = new ArrayList<>();
+		
+		ArrayList<Integer> selectList = new ArrayList<>(); // 보기 인덱스 관리 리스트
+		
 		Random ran = new Random();
 		
 		int count = 0;
@@ -25,17 +28,32 @@ public class 수도퀴즈 {
 			break;
 		}
 		for(int i=1; i<=count; i++) {
-			
+			selectList.clear();
 			int ranNum = ran.nextInt(quizList.size());
 			Quiz quiz = quizList.get(ranNum);
 			if(questionList.contains(quiz.getCountry())) {
 				i--;
 				continue;
 			}
+			int correctIndex = ran.nextInt(4); // 리스트에서 정답의 위치 랜덤하게 0~3
+			for(int j=0; j<4; j++) {
+				int index = ran.nextInt(quizList.size());
+				if(correctIndex == j) { // 정답 위치와 j가 같을 때 보기에 추가
+					selectList.add(ranNum);
+				} else if(selectList.contains(index) || index == ranNum) { // 이미 보기에 있거나 정답과 같을 경우 다시 시도
+					j--;
+				} else { // 정답 외 값들 보기에 추가
+					selectList.add(j, index);
+				}
+			}
 			
-			System.out.print(i + ") " + quiz.getCountry() + " : ");
-			String answer = s.next();
-			if(answer.equals(quiz.getCapital())) {
+			System.out.println(i + ") " + quiz.getCountry() + " 수도? ");
+			for(int j=0; j<4; j++) { // 리스트에 있는 index 기준으로 보기 작성
+				System.out.print("["+(j+1)+"] "+quizList.get(selectList.get(j)).getCapital() + " ");
+			}
+			System.out.print(": ");
+			int answer = s.nextInt();
+			if(quizList.get(selectList.get(answer-1)).getCapital().equals(quiz.getCapital())) {
 				System.out.println("정답!");
 			} else {
 				System.out.println("오답!"); 
